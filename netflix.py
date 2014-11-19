8b0271cfac18813ab7c7dfbefbeceb0aa9174028
Suneel Chakravorty <suneel0101@gmail.com>
2014-11-19 12:02:13 -0500

    updating netflix with solution

--- netflix.py
+++ netflix.py
@@ -1,7 +1,4 @@
-# Problem 1: create function that takes a list of movies and returns the one with the highest fb fan count
-# Problem 2: create function that takes a list of movies and returns the one with the highest imdb_score
-
-movies = [
+fav_movies = [
     {
         "name": "Shawshank Redemption",
         "imdb_score": 8.7,
@@ -18,3 +15,26 @@ movies = [
         "fb_fan_count": 1200000,
     }
 ]
+
+def get_best_fb_movie(movies):
+    # pick the first one to start
+    best_movie = movies[0]
+    for movie in movies:
+        # if `movie` has a better fb_fan_count
+        # make that our best movie so far
+        if movie["fb_fan_count"] > best_movie["fb_fan_count"]:
+            best_movie = movie
+    return best_movie
+
+def get_best_imdb_score(movies):
+    best_movie = movies[0]
+    for movie in movies:
+        if movie["imdb_score"] > best_movie["imdb_score"]:
+            best_movie = movie
+    return best_movie
+
+print "best imdb score"
+print get_best_imdb_score(fav_movies)
+
+print "best fb score"
+print get_best_fb_movie(fav_movies)
