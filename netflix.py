535d0d62814d9d8eaf942a27348416436b7a3b55
Suneel Chakravorty <suneel0101@gmail.com>
2014-11-19 11:36:06 -0500

    adding netflix movies

---
+++ netflix.py
@@ -0,0 +1,17 @@
+movies = [
+    {
+        "name": "Shawshank Redemption",
+        "imdb_score": 8.7,
+        "fb_fan_count": 900000
+    },
+    {
+        "name": "Never Say Never",
+        "imdb_score": 1.2,
+        "fb_fan_count": 80000000,
+    },
+    {
+        "name": "Interstellar",
+        "imdb_score": 7.7,
+        "fb_fan_count": 1200000,
+    }
+]
